d992a2e8207a58a5e727a3b2202e5faca14d4d4e
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2020-02-10 12:17:54 +0900

    log4js log

--- src/controllers/User/User.ts
+++ src/controllers/User/User.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from 'express';
+import log4js from 'log4js';
 import { CrudController } from '../CrudController';
+
+const logger = log4js.getLogger();
+logger.level = 'debug';
+
 
 export class UserController extends CrudController {
   public create(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): void {
@@ -8,6 +13,17 @@ export class UserController extends CrudController {
 
   public read(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): void {
     res.json({ message: 'GET /user request received' });
+    const user = {
+      id: 1,
+      name: 'hogehgoe',
+      age: 23,
+    }
+    const users = [
+      { id: 1, name: 'yamada taro', age: 23 },
+      { id: 2, name: 'wakabayashi ichiro', age: 30 },
+    ]
+    logger.debug('get user request: ' + JSON.stringify(users))
+    // logger.debug('get user request: ' + JSON.stringify(user))
   }
 
   public update(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): void {
